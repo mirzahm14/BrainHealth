74ce3df93925feb8ea3f45623ce54eadaf3f6a26
Mirza Hafiz Muhammad <88041796+mirzahm14@users.noreply.github.com>
2022-11-17 09:37:58 +0700

    Update register.php

--- basdat/register.php
+++ basdat/register.php
@@ -23,15 +23,11 @@ if (isset($_POST['submit'])&&!empty($_POST['submit'])){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>PHP PostgreSQL Registration & Login Example </title>
-  <meta name="keywords" content="PHP,PostgreSQL,Insert,Login">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <title>Registrasi Akun </title>
 </head>
 <body>
 <div class="container">
-  <h2>Register Here </h2>
+  <h2>Daftar disini </h2>
   <form method="post">
   
     <div class="form-group">
