7745c456aa8691c13343cc2ab416805cd61afd66
Mirza Hafiz Muhammad <88041796+mirzahm14@users.noreply.github.com>
2022-11-17 09:38:26 +0700

    Update login.php

--- basdat/login.php
+++ basdat/login.php
@@ -20,15 +20,11 @@ if(isset($_POST['submit'])){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>PHP PostgreSQL Registration & Login Example </title>
-  <meta name="keywords" content="PHP,PostgreSQL,Insert,Login">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <title>Login</title>
 </head>
 <body>
 <div class="container">
-  <h2>Login Here </h2>
+  <h2>Masuk </h2>
   <form method="post">
   
      
