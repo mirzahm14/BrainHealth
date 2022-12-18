05cfdb301ecc8b7ed89d4ec59b9aadaddf844cf9
Mirza Hafiz Muhammad <88041796+mirzahm14@users.noreply.github.com>
2022-12-18 11:38:45 +0700

    upload ulang projek utk terakhir kalinya

--- projek/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $server = "localhost";
-    $user = "root";
-    $password ="";
-    $database = "konsultasi";
-
-    $koneksi = mysqli_connect($server,$user,$password,$database) or die(mysqli_error($koneksi));
-?>
